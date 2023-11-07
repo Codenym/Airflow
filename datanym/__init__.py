c17304b721526148cbe87bee2a2f78621f3f478d
Isaac Flath <Isaac.flath@gmail.com>
2023-11-07 06:40:13 -0500

    move resources to it's own directory

--- datanym/__init__.py
+++ datanym/__init__.py
@@ -1,8 +1,7 @@
 from dagster import Definitions, load_assets_from_modules
 
-
 from .assets.IRS527 import irs_527
-from .assets.resources import local_io_manager
+from .resources.local_io_manager import local_io_manager
 
 defs = Definitions(
     assets=load_assets_from_modules([irs_527]),
