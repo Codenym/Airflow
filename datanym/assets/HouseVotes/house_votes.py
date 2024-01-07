198006372004d8313dcd9f265a29e69e533c9594
fflath <felix@fflath.email>
2024-01-07 08:18:38 -0500

    landing works

--- datanym/assets/HouseVotes/house_votes.py
+++ datanym/assets/HouseVotes/house_votes.py
@@ -15,16 +15,20 @@ from typing import Dict, List
 import json
 import yaml
 
+import pandas as pd 
 from copy import deepcopy
+from functools import partial
 
 from dagster import (asset,
                      AssetOut,
                      multi_asset,
                      get_dagster_logger,
+                     StaticPartitionsDefinition
                      )
 from . import votes
 from git import Repo
 
+download_sessions = [110, 111, 112, 113, 114, 115, 116, 117, 118]
 
 @asset(io_manager_key="local_io_manager",group_name="house_assets")
 def bulk_download() -> Path:
@@ -34,78 +38,123 @@ def bulk_download() -> Path:
     """
 
     base_dir = Path("data/sessions")
-    for congress in [110, 111, 112, 113, 114, 115, 116, 117, 118]:
+    for congress in download_sessions:
+        if os.path.isdir(base_dir / str(congress)):
+            continue
         opts = {"congress": congress, "chamber": "house"}
         votes.run(opts)
         shutil.move("data/" + str(congress), base_dir / str(congress))
     return base_dir
 
 
-@multi_asset(
-        outs = {
-            "landing_house_votes": AssetOut(io_manager_key="DuckPondIOManager"),
-            "landing_house_reps": AssetOut(io_manager_key="DuckPondIOManager"),
-            "landing_house_vote_transaction": AssetOut(io_manager_key="DuckPondIOManager"),
-        },group_name="house_assets")
-def parse_votes(bulk_download) -> tuple[SQL, SQL, SQL]:
-    """
-    Parses the votes from the bulk download.
+# @multi_asset(
+#         partition_defs=StaticPartitionsDefinition([str(i) for i in download_sessions]),
+#         outs = {
+#             "landing_house_votes": AssetOut(io_manager_key="DuckPondIOManager"),
+#             "landing_house_reps": AssetOut(io_manager_key="DuckPondIOManager"),
+#             "landing_house_vote_transaction": AssetOut(io_manager_key="DuckPondIOManager"),
+#         },group_name="house_assets")
+# def parse_votes(bulk_download) -> tuple[SQL, SQL, SQL]:
+#     """
+#     Parses the votes from the bulk download.
 
-    Creates three lists:
-    - reps: list of representatives
-    - votes: list of votes
-    - vote_transaction: list of how each representative voted
+#     Creates three lists:
+#     - reps: list of representatives
+#     - votes: list of votes
+#     - vote_transaction: list of how each representative voted
 
-    """
-    logger = get_dagster_logger()
-    votes = []
-    vote_transaction = []
-    reps = []
-    vote_cat = defaultdict(list)
-    vote_keys = [ 
-        'bill_number','bill_type',
-        'amendment_author','amendment_number','amendment_type',
-        'category','chamber','congress','date','number','question','requires','result','result_text',
-        'session','source_url','subject','type','updated_at','vote_id']
-    for file in Path("data/sessions/").glob("**/*.json"):
-        with open(file) as f:
-            data = json.load(f)
+#     """
+#     logger = get_dagster_logger()
+#     votes = []
+#     vote_transaction = []
+#     reps = []
+#     vote_cat = defaultdict(list)
+#     vote_keys = [ 
+#         'bill_number','bill_type',
+#         'amendment_author','amendment_number','amendment_type',
+#         'category','chamber','congress','date','number','question','requires','result','result_text',
+#         'session','source_url','subject','type','updated_at','vote_id']
+#     for file in Path("data/sessions/").glob("**/*.json"):
+#         # print(file)
+#         with open(file) as f:
+#             data = json.load(f)
 
-        try:
-            data['bill_number'] = data['bill']['number']
-            data['bill_type'] = data['bill']['type']
-        except: 
-            data['bill_number'] = None
-            data['bill_type'] = None
+#         try:
+#             data['bill_number'] = data['bill']['number']
+#             data['bill_type'] = data['bill']['type']
+#         except: 
+#             data['bill_number'] = None
+#             data['bill_type'] = None
 
-        try:
-            data['amendment_author'] = data['amendment']['author']
-            data['amendment_number'] = data['amendment']['number']
-            data['amendment_type'] = data['amendment']['type']
+#         try:
+#             data['amendment_author'] = data['amendment']['author']
+#             data['amendment_number'] = data['amendment']['number']
+#             data['amendment_type'] = data['amendment']['type']
             
-        except:
-            data['amendment_author'] = None
-            data['amendment_number'] = None
-            data['amendment_type'] = None 
+#         except:
+#             data['amendment_author'] = None
+#             data['amendment_number'] = None
+#             data['amendment_type'] = None 
         
-        vote_cat[data['category']].append(data)
-        vote = {}
-        for key in vote_keys:
-            vote[key]=data.get(key, None)
-        votes.append(vote)
+#         vote_cat[data['category']].append(data)
+#         vote = {}
+#         for key in vote_keys:
+#             vote[key]=data.get(key, None)
+#         votes.append(vote)
 
-        rs=[(vote,key) for key in data["votes"].keys() for vote in data["votes"][key]]
-        vote = {}
-        for (v,key) in rs:
-            vote['voter_id'] = v['id']
-            vote['vote'] = key
-            vote['vote_id'] = data['vote_id']
-            vote_transaction.append(deepcopy(vote))
+#         rs=[(vote,key) for key in data["votes"].keys() for vote in data["votes"][key]]
+#         vote = {}
+#         for (v,key) in rs:
+#             vote['voter_id'] = v['id']
+#             vote['vote'] = key
+#             vote['vote_id'] = data['vote_id']
+#             vote_transaction.append(deepcopy(vote))
 
-        rep=[vote for key in data["votes"].keys() for vote in data["votes"][key]]
-        reps = reps + rep
+#         rep=[vote for key in data["votes"].keys() for vote in data["votes"][key]]
+#         reps = reps + rep
 
-    return tuple(SQL('select * from $df', df=pd.DataFrame(itm)) for itm in tuple([votes, reps, vote_transaction]))
+#     return tuple(SQL('select * from $df', df=pd.DataFrame(itm)) for itm in tuple([votes, reps, vote_transaction]))
+
+
+# parse an individual vote file
+def parse_votes_file(file: Path):
+    with open(file) as f:
+        raw = json.load(f)
+
+    def create_votes(vote_type='Yea', vote_id=''):
+        df = pd.DataFrame(raw['votes'][vote_type])
+        df['vote_type'] = vote_type
+        df['vote_id'] = vote_id
+        return df
+
+    create_votes = partial(create_votes, vote_id=raw['vote_id'])
+    vote_transaction = pd.concat(list(map(create_votes,raw['votes'])))
+
+    try:
+        df = pd.DataFrame(raw['bill'],index=[0])
+        df.columns = ['bill_'+c for c in df.columns]
+    except:
+        df = pd.DataFrame(index=[0])
+    no_votes = {k:v for k,v in raw.items() if k not in ['votes','bill']}
+    vote = pd.concat([pd.DataFrame(no_votes,index=[0]),df],axis=1)
+    return vote_transaction, vote
+
+@multi_asset(
+    outs = {
+        "landing_house_votes": AssetOut(io_manager_key="DuckPondIOManager"),
+        "landing_house_votes_transactions": AssetOut(io_manager_key="DuckPondIOManager")},
+    group_name="house_assets",
+)
+def parse_votes(bulk_download) -> tuple[SQL, SQL]:
+    logger = get_dagster_logger()
+    vote_transactions = []
+    votes = []
+    for file in Path("data/sessions/").glob("**/*.json"):
+        vt, v = parse_votes_file(file)
+        vote_transactions.append(vt)
+        votes.append(v)
+    return tuple(SQL('select * from $df', df=pd.concat(itm)) for itm in tuple([votes,vote_transactions]))
+    
 
 @asset(group_name="house_assets")
 def house_reps_download():
@@ -131,31 +180,50 @@ def house_reps_download():
 def parse_reps(house_reps_download) -> tuple[SQL, SQL]:
     logger = get_dagster_logger()
 
-    with open("data/congress-legislators/legislators-current.yaml","r") as f:
-        raw = yaml.safe_load(f)
 
     reps = []
     terms = []
-    for rep in raw:
-        temp = {'first': rep['name']['first'],
-                'last': rep['name']['last'],
-                'official_full': rep['name'].get('official_full', None),
-                'birthday': rep['bio']['birthday'],
-                'gender': rep['bio']['gender']}
 
-        for k,v in rep['id'].items(): temp[k]=v
+    id_cols = ["bioguide","thomas","lis","govtrack","opensecrets","votesmart","fec",
+               "cspan","wikipedia","house_history","ballotpedia","maplight","icpsr",
+               "wikidata","google_entity_id","wikipedia","wikidata"]
+    
+    for file in ["data/congress-legislators/legislators-current.yaml","data/congress-legislators/legislators-historical.yaml"]:
+        with open(file,"r") as f:
+            raw = yaml.safe_load(f)
 
-        reps.append(deepcopy(temp))
-        for t in rep['terms']:
+        for rep in raw:
+            temp = {'first': rep['name']['first'],
+                    'last': rep['name']['last'],
+                    'official_full': rep['name'].get('official_full', ''),
+                    'birthday': rep['bio'].get('birthday', ''),
+                    'gender': rep['bio']['gender']}
 
-            if t['type'] == 'sen':
-                continue
+            for col in id_cols:
+                temp[col] = rep['id'].get(col, '')
+            # for k,v in rep['id'].items(): temp[k]=v
 
-            cols = ['url','caucus','address','office','phone','fax','contact_form','party_affiliations','rss_url','how']
-            for col in cols:
-                if not t.get(col,False): t[col] = ""
+            reps.append(deepcopy(temp))
+            for t in rep['terms']:
 
-            t['official_full'] = rep['name'].get('official_full', None)
-            t['bioguide'] = rep['id']['bioguide']
-            terms.append(deepcopy(t))
+                if t['type'] == 'sen':
+                    continue
+
+                cols = ['url','caucus','address','office','phone','fax','contact_form','party_affiliations','rss_url','how']
+                for col in cols:
+                    if not t.get(col,False): t[col] = ''
+
+                t['official_full'] = rep['name'].get('official_full', '')
+                t['bioguide'] = rep['id']['bioguide']
+                terms.append(deepcopy(t))
     return tuple(SQL('select * from $df', df=pd.DataFrame(itm)) for itm in tuple([reps, terms]))
+
+
+# @asset(group_name="house_assets",io_manager_key="DuckPondIOManager")
+# def staging_house_votes(landing_house_votes):
+#     sql_template = load_sql_file(sql_file=Path("datanym/assets/HouseVotes/sql_scripts/curated_form8872_transactions.sql"))
+#     return SQL(sql_template,
+#                staging_form8872_contributions=staging_form8872_contributions,
+#                staging_form8872_expenditures=staging_form8872_expenditures
+#                )
+
